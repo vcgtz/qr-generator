fd40bd2a1abbaeb8cc07fa19083eb6ef9a5c02ce
Vicente Gutiérrez <gtz.rvera@gmail.com>
2024-05-07 22:29:00 -0600

    add typer to improve the CLI interface

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-def greet():
-    print("Hello World!")
+import typer
+from rich import print
+
+app = typer.Typer()
+
+
+@app.command()
+def create(text: str):
+    print(f"Text: {text}")
+
+
+@app.command()
+def version():
+    print("\nQR Generator CLI")
+    print("[bold magenta]v0.0.1[/bold magenta]")
 
 
 if __name__ == "__main__":
-    greet()
+    app()
